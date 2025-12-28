67dafad34885a3873b240a5f0cacc12bfbc91346
Mateusz Seler <mateusz.seler@gmail.com>
2025-12-28 17:49:05 +0100

    refactor

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -14,27 +14,19 @@ export interface Figure {
 export class Triangle implements Figure {
   shape: Shape = 'triangle';
 
-  color: Color;
-
-  a: number;
-
-  b: number;
-
-  c: number;
-
-  constructor(color: Color, a: number, b: number, c: number) {
-    this.a = a;
-    this.b = b;
-    this.c = c;
-    this.color = color;
-
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
     if (a <= 0 || b <= 0 || c <= 0) {
-      throw new Error('side must be greater than 0');
+      throw new Error('side of Triangle must be greater than 0');
     }
 
     if (Math.max(a, b, c) >= a + b + c - Math.max(a, b, c)) {
       throw new Error(
-        'the longest side of a triangle is >= than a sum of two others',
+        'the longest side of Triangle triangle is >= than a sum of two others',
       );
     }
   }
@@ -50,14 +42,10 @@ export class Triangle implements Figure {
 export class Circle implements Figure {
   shape: Shape = 'circle';
 
-  color: Color;
-
-  radius: number;
-
-  constructor(color: Color, radius: number) {
-    this.radius = radius;
-    this.color = color;
-
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
     if (radius <= 0) {
       throw new Error('radius must be greater than 0');
     }
@@ -71,19 +59,13 @@ export class Circle implements Figure {
 export class Rectangle implements Figure {
   shape: Shape = 'rectangle';
 
-  width: number;
-
-  height: number;
-
-  color: Color;
-
-  constructor(color: Color, width: number, height: number) {
-    this.width = width;
-    this.height = height;
-    this.color = color;
-
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
     if (width <= 0 || height <= 0) {
-      throw new Error('side must be greater than 0');
+      throw new Error('sides of  Rectangle must be greater than 0');
     }
   }
 
